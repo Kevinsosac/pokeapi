829ef950b2427fa0dbb49587b469b1eebbb2d81b
halg830 <halg830@gmail.com>
2023-10-03 14:31:44 -0500

    nuevo

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,6 @@
             </section>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
           </div>
         </div>
       </div>
